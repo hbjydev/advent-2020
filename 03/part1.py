1fe4443aa02b2fcba9efbee68fd15a02945141c9
Hayden Young <hi@hbjy.dev>
2020-12-05 23:27:47 +0000

    feat: half-baked implementation of d3p1

---
+++ 03/part1.py
@@ -0,0 +1,28 @@
+input = []
+
+with open('input.txt', 'r') as file:
+    input = file.read().splitlines()
+
+y = 0 # Vertical Position (used for progress)
+x = 0 # Horizonal Position (used for scroll tracking)
+
+finish = False
+
+trees = 0
+
+while finish == False:
+    position = input[y][x]
+    if position == '#':
+        trees += 1
+
+    if x + 3 >= 31:
+        x = 0
+    else:
+        x += 3
+
+    if y + 1 == len(input):
+        finish = True
+    else:
+        y += 1
+
+print(trees)
